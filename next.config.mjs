3c778476e02557862961c1bdca16eda78a59f87b
Leo <leovilis@gmail.com>
2026-03-25 12:04:17 -0300

    Fix: remove static export, add manifest.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   trailingSlash: true,
-//   images: {
-//     unoptimized: true
-//   },
-//   // Configuraciones adicionales para Cordova
-//   assetPrefix: '/',
-//   basePath: '',
-//   distDir: 'out'
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true
   }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
